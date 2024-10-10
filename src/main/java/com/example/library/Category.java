169cab08f01d12c9cefc6cc02057f69096b48eec
Jonatan <jonatanav255@hotmail.com>
2024-10-10 07:58:24 -0600

    adding category

---
+++ src/main/java/com/example/library/Category.java
@@ -0,0 +1,23 @@
+package com.example.library;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category") // One category can include many books
+    private List<Book> books;
+
+    // Constructors, Getters, Setters
+}
