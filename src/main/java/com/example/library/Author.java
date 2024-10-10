3df1ec522311c8ec69193e863e99e26d63745fe7
Jonatan <jonatanav255@hotmail.com>
2024-10-10 08:08:19 -0600

    Changes to author

--- src/main/java/com/example/library/Author.java
+++ src/main/java/com/example/library/Author.java
@@ -7,16 +7,43 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+
 
 @Entity
 public class Author {
        
-                @Id
-                @GeneratedValue(strategy = GenerationType.IDENTITY)
-                private Long id;
-                private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-                @OneToMany(mappedBy="author")
-                private List<Book> books;
+    private String name;
 
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Book> books;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 }
